4747e33a3973ba627e045e331737a781b2fb1ca8
Aleksandr-odessa <p_alecs@mail.ru>
2024-05-01 12:34:12 +0200

    v2.2

--- offices_translate.py
+++ offices_translate.py
@@ -1,18 +1,17 @@
 import datetime
 import calendar
 import os
-
-from imap_tools import MailBox, AND
-from config import mail_and_name, name_and_mail, year
 import re
-import logging
-from time import time
+from imap_tools import MailBox, AND
+from config import year, name_and_mail, mail_and_name
+
+# import logging
+# from time import time
 
 IMAP: str = os.environ['SERVER_IMAP']
 MAIL_USERNAME: str = os.environ['USERNAME_MAIL']
 MAIL_PASSWORD: str = os.environ['PASS_MAIL']
 
-list_data = []
 # file_log = logging.FileHandler("mailLog.log", "w")
 # console_out = logging.StreamHandler()
 # logging.basicConfig(handlers=(file_log, console_out),
@@ -20,105 +19,62 @@ list_data = []
 #                     level=logging.INFO)
 
 
-def select_month_day(data: str) -> tuple:
-    if data.isalnum():
-        month: int = int(data)
-        day_start: int = 1
-    else:
-        day_month: list = data.split(".")
-        month: int = int(day_month[1])
-        day_start: int = int(day_month[0])
-    return day_start, month
+class GetCountForMonth():
+    def __init__(self, data: list):
+        self.data = data
+        self.message_list = []
 
+    def create_list_of_data(self) -> list:
+        month: int = int(self.data[0])
+        start_day: int = int(self.data[1])
+        day_finish: int = calendar.monthrange(year, month)[1]+1
+        # creating a list of days
+        list_of_days: list = [datetime.date(year, month, i) for i in range(start_day, day_finish)]
+        return list_of_days
 
-def request_mail(data: list) -> list:
-    # data = [translate office, date]
-    message_list = []
-    month_day: tuple = select_month_day(data[0])
-    month: int = month_day[1]
-    day_finish: int = calendar.monthrange(year, month)[1]
-    # # creating a list of days
-    list_of_days: list = [datetime.date(year, month, i) for i in range(month_day[0], day_finish + 1)]
-    len_data: int = len(data)
-    # creating to connect to server
-    start_time = time()
-    with MailBox(IMAP).login(MAIL_USERNAME, MAIL_PASSWORD) as mailbox:
-        # select the folder
-        mailbox.folder.set('Отправленные')
-        if len_data >= 2:
-            mail = name_and_mail.get(data[1])
-            for day in list_of_days:
-                for msg in mailbox.fetch(AND(date=day, to=mail)):
-                    message_list.append([msg.uid, str(msg.date), msg.to, msg.text])
-        else:
-            for day in list_of_days:
-                for msg in mailbox.fetch(AND(date=day)):
-                    message_list.append([msg.uid, str(msg.date), msg.to, msg.text])
-    end_time = time()
-    res_time = end_time-start_time
-    print(f'res_time={res_time}')
-    return message_list
-
-
-def request_mail2(list_of_days: list, data: list) -> list:
-    len_data: int = len(data)
-    start_time = time()
-    if len_data < 2:
+    def request_mail(self) -> list:
+        # creating to connect to server
         with MailBox(IMAP).login(MAIL_USERNAME, MAIL_PASSWORD) as mailbox:
             # select the folder
             mailbox.folder.set('Отправленные')
-            for day in list_of_days:
-                message_list: list = [[msg.uid, str(msg.date), msg.to, msg.text] for msg in mailbox.fetch(AND(date=day))]
-    else:
-        mail = name_and_mail.get(data[1])
+            for day in self.create_list_of_data():
+                for msg in mailbox.fetch(AND(date=day, text="гривен"), charset='utf-8'):
+                    self.message_list.append([msg.uid, str(msg.date), msg.to, msg.text])
+        return self.message_list
+
+    @staticmethod
+    def get_money(temp_list: list) -> list:
+        cleared_list = []
+        for mes in temp_list:
+            date: str = re.search(r'\d{4}-\d{2}-\d{2}', mes[1])[0]
+            split_money: str = mes[3].split('гривен')[0]
+            equally: int = split_money.find('=')
+            money: str = split_money[equally+2:] if equally != -1 else re.search(r'\d{2,}', split_money)[0]
+            cleared_list.append([mes[0], date, mes[2][0], money])
+        return cleared_list
+
+    def request_summ(self) -> str:
+        list_message: list = self.request_mail()
+        messages: list = self.get_money(list_message)
+        get_email: list = [_[2] for _ in messages]
+        dict_of_messengers = {}
+        for email in get_email:
+            temp: list = [int(mes[3]) for mes in messages if email in mes]
+            string: str = "  ".join(["суммы:", str(temp), "общая сумма", str(sum(temp))])
+            dict_of_messengers[mail_and_name.get(email)] = string
+        return str(dict_of_messengers)
+
+
+class GetCountForPeriod(GetCountForMonth):
+
+    def request_mail(self) -> list:
+        mail: str = name_and_mail.get(self.data[2])
+        # creating to connect to server
         with MailBox(IMAP).login(MAIL_USERNAME, MAIL_PASSWORD) as mailbox:
-        # select the folder
+            # select the folder
             mailbox.folder.set('Отправленные')
-            for day in list_of_days:
-                message_list: list = [[msg.uid, str(msg.date), msg.to, msg.text] for msg in mailbox.fetch(AND(date=day, to=mail))]
-    end_time = time()
-    res_time = end_time - start_time
-    print(f'res_time2={res_time}')
-    return message_list
-
-def create_list_of_days(data:list) -> list:
-    month_day: int = select_month_day(data[0])[0]
-    month: int = month_day[1]
-    day_finish: int = calendar.monthrange(year, month)[1]+1
-    # creating a list of days
-    list_of_days: list = [datetime.date(year, month, i) for i in range(month_day, day_finish)]
-    return list_of_days
-
-
-def get_money(list_message: list) -> list:
-    find_word: str = "гривен"
-    cleared_list = []
-    temp_list = [mes for mes in list_message if find_word in mes[3]]
-    for mes in temp_list:
-        date: str = re.search(r'\d{4}-\d{2}-\d{2}', mes[1])[0]
-        money: str = mes[3].split('гривен')[0]
-        equally: int = money.find('=')
-        money: str = money[equally+2:] if equally != -1 else re.search(r'\d{2,}', money)[0]
-        cleared_list.append([mes[0], date, mes[2][0], money])
-    return cleared_list
-
-
-def request_summ(data: str) -> str:
-    start_time = time()
-    list_message: list = request_mail(data)
-    messages: list = get_money(list_message)
-    set_email: list = [i[2] for i in messages]
-    dict_of_messengers = {}
-    for email in set_email:
-        temp: list = [int(mes[3]) for mes in messages if email in mes]
-        string: str = "  ".join(["суммы:", str(temp), "общая сумма", str(sum(temp))])
-        dict_of_messengers[mail_and_name.get(email)] = string
-    finish_time = time()
-    res_time = finish_time - start_time
-    print(res_time)
-    return str(dict_of_messengers)
-
-
-def check_data(data: str) -> bool:
-    return bool(re.fullmatch(r'\d{1,2}\.\d{1,2}', data))
-
+            for day in self.create_list_of_data():
+                for msg in mailbox.fetch(AND(date=day, text="гривен", to=mail), charset='utf-8'):
+                    # self.message_list.append([msg.uid, str(msg.date), msg.text])
+                    self.message_list.append([msg.uid, str(msg.date), msg.to, msg.text])
+        return self.message_list
